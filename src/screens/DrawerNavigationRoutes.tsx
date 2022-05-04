0102f104ff8895e76a8c755e333666d97289f56c
craftomeCJ <tech@craftome.com>
2022-05-04 23:37:07 +0800

    app setup part 5 additional dependencies & folder

---
+++ src/screens/DrawerNavigationRoutes.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DrawerNavigationRoutes = () => {
+  return (
+    <View>
+      <Text>DrawerNavigationRoutes</Text>
+    </View>
+  )
+}
+
+export default DrawerNavigationRoutes
+
+const styles = StyleSheet.create({})
